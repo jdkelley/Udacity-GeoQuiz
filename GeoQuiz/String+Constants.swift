fed30b7612a1c476584a48429a02b9acd420f710
Joshua Kelley <jdkelley@samaritan.org>
2016-06-24 16:08:30 -0400

    Add Files from Udacity project. Modify QuizViewController with if-else to notify user if choice was correct. Add String+Constants to mitigate magic strings.

---
+++ GeoQuiz/String+Constants.swift
@@ -0,0 +1,25 @@
+//
+//  String+Constants.swift
+//  GeoQuiz
+//
+//  Created by Joshua Kelley on 6/24/16.
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    struct Alert {
+        
+        struct Title {
+            static let correct: String = "Correct"
+            static let incorrect: String = "Incorrect"
+        }
+        
+        struct Text {
+            static let correct: String = "Right on!"
+            static let incorrect: String = "Nope. Try again"
+        }
+    }
+}
